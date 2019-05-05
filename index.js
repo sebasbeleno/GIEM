1dc5da676b0e5f514838e18b21167c1b9ee2ceb5
Beleño <sebasbeleno15@gmail.com>
2019-05-05 14:16:36 -0500

    Agregar comantarios
    Agrege comentarios a los archivos del servidor. No se modifico
    nada interno y/o funcionamiento del server
    
    Esto con el objetivo de que entiendan cada linea de codigo.
    & se pueda leer el codigo a futuro de una mejor forma.

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
+/** Importacion de modulos */
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const path = require('path');
 const colors = require('colors');
+
+/*
+Usamos en omodulo de Mirgan para que nos imprima por consola
+las petiociones del usuario.
+*/
 app.use(morgan('short'));
 
 
+/** Guardamos en una const lo qye no triga nuestro
+ * archivo de rutas llamado 'routes'
+ */
 const routes  = require('./routes');
 
 
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
+
+/* Le decimos a nuestro server que use las rutas que  
+ * anteriormente llamamos en la variable de mismo nombre
+*/
 app.use(routes);
 
+
+/** iniciamos el server en el puerto 300m, una vez
+ * inicado le decimos que ejecute una funcion que 
+ * imprima por consola "server en el puerto 3000"
+ */
 app.listen(3000, function () {
     console.log('Server on port 3000'.blue);
 });
--- routes.js
+++ routes.js
@@ -1,8 +1,18 @@
+/** Importacion de modulos */
 const express = require('express');
+/** En este caso no ncesitamos todo el modulo de express
+ * solo ocupamos su objeto de ritas, que es lo que 
+ * haremos en este archivo
+  */
 const routes = express.Router();
 const path = require('path');
 
 
+/** Le decimos las rutas que querremos añadir a nuestro
+ * servidor y su arcvhio de respuesta, si quieren más
+ * imformacion sobre estas lineas me pueden llamar.
+ * Esto es un poco más largo
+ */
 routes.get('/', (req, res  ) => {
     res.sendFile('index.html', {
         root: path.join(__dirname, '/public')
@@ -12,7 +22,7 @@ routes.get('/', (req, res  ) => {
 
 
  
- routes.get('/academica', (req, res  ) => {
+routes.get('/academica', (req, res  ) => {
     res.sendFile('academica.html', {
         root: path.join(__dirname, '/public')
     })
@@ -26,4 +36,7 @@ routes.get('/deportes', (req, res  ) => {
 })
 
 
+/** Exportamos todo nuestro metodo routes 
+ * para que se pueda usar en otros arvhivos. 
+ */
 module.exports = routes;
