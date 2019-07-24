12632fce96ae381a3e59b8a89c0e45ed965921bc
Sebastian Beleño <sebasbeleno15@gmail.com>
2019-07-23 20:15:22 -0500

    Taks: Notificacion por email al estudiante

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const session = require('express-session');
 
 //Url de la base de datos
 const { url } = require('./config/database.js');
-
+const nodeMailer = require('nodemailer')
 mongoose.connect(url, {
 	
 });
@@ -30,7 +30,7 @@ app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
+var ejs = require("ejs");
 
 /*
 Usamos en omodulo de Mirgan para que nos imprima por consola
@@ -65,20 +65,69 @@ app.post('/addEstudiante', (req, res) => {
 		if (user) {
 			return done(null, false, req.flash('signupMessage', 'Lastima... Este email ya existe.'));
 		}else{
-			let estudiantes = new Estudiantes();
 
+			
+			
+			
+			let estudiantes = new Estudiantes();
+			
 			estudiantes.estudiantes.nombre = req.body.name;
 			estudiantes.estudiantes.correo = req.body.email;
 			estudiantes.estudiantes.psicoEmail =  req.body.psicoEmail;
 			estudiantes.estudiantes.password = req.body.ti;
 			
-
+			
 			estudiantes.estudiantes.datos = false
-
+			
 			estudiantes.save(function (err) {
 				if (err) res.send(err)
+				/**
+				 * 
+				 * NODEMAILER para el estudiante 
+				 * 
+				 */
+
+				 
+				let transporter = nodeMailer.createTransport({
+					/** protocolos que usaremos, en este caso, smtp de google */
+					host: 'smtp.gmail.com',
+					port: 465,
+					secure: true,
+					auth: {
+						/** La cuenta desde donde enviaremos el correo, en este caso
+						 * GIEMJS
+						 */
+						user: 'inemgiem@gmail.com',
+						pass: 'GIEM2019'
+					}
+				});
+
+			
+
+				ejs.renderFile(__dirname + "/public" + "/html" +  "/emailTemplate.ejs", { estudiante: req.body }, function (err, data) {
+					if (err) {
+						console.log(err);
+					}else {
+						var mainOptions = {
+							to: req.body.email,
+							subject: "Has sido registrado en GIEM corractamente",
+							html: data
+						};
+						console.log("html data ======================>", mainOptions.html);
+						transporter.sendMail(mainOptions, function (err, info) {
+							if (err) {
+								console.log(err);
+							} else {
+								//console.log('Message sent: ' + info.response);
+								res.redirect('/estudiantes')
+							}
+						});
+					}
+				})	 
+
 				
-				res.redirect('/estudiantes')
+
+				
 			});
 		}
 	})
